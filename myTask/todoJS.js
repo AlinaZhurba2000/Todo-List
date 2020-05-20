0943d05050b3bb6cb0fab8809e59b3e07b782edd
AlinaZhurba2000 <zhurbaalina@mail.ru>
2020-05-20 20:36:30 +0300

    фф

--- myTask/todoJS.js
+++ myTask/todoJS.js
@@ -1,20 +1,2 @@
 "use strict"
 
-let tasks = [];
-
-function addTask()
-{
-    let task = {
-        task: value,
-        priority: value,
-        status: value,
-        date: value,
-    };
-    tasks.push(task);
-    return tasks;
-}
-
-function deleteTask(tasks)
-{
-    tasks.splice()
-}
