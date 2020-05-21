3710920f0f96afb46df9a4550b3105380dae4587
AlinaZhurba2000 <zhurbaalina@mail.ru>
2020-05-21 19:26:49 +0300

    asas

--- myTask/todoJS.js
+++ myTask/todoJS.js
@@ -15,19 +15,19 @@ class task{
 
         let input = document.createElement('input');
     	input.type = "text";
-    	input.disabled = true;
+        input.disabled = true;
     	input.value = name;
-    	input.classList.add('item');
-
+        input.classList.add('item');
+        
     	let cancelTask = document.createElement('button');
     	cancelTask.classList.add('itemCancel');
-    	cancelTask.innerHTML = "Cancel";
+    	cancelTask.innerHTML = "Edit";
         cancelTask.addEventListener('click', () => this.edit(input));
         
-        let doneTask = document.createElement('button');
-    	doneTask.classList.add('itemCancel');
-    	doneTask.innerHTML = "Cancel";
-    	doneTask.addEventListener('click', () => this.edit(input));
+        //let doneTask = document.createElement('button');
+    	//doneTask.classList.add('itemCancel');
+    	//doneTask.innerHTML = "Cancel";
+    	//doneTask.addEventListener('click', () => this.edit(input));
 
     	let deleteTask = document.createElement('button');
     	deleteTask.classList.add('itemDone');
@@ -39,12 +39,10 @@ class task{
         itemBox.appendChild(input);
         itemBox.appendChild(cancelTask);
         itemBox.appendChild(deleteTask);
+
     }
     edit(input){
-      
            input.disabled = !input.disabled;
-
-    	
     }
 
     remove(itemBox){
