66384b3cb1e98faf2fc0c52c939940198465fba7
AlinaZhurba2000 <62932346+AlinaZhurba2000@users.noreply.github.com>
2020-05-21 16:37:42 +0300

    sdsd

--- myTask/todoJS.js
+++ myTask/todoJS.js
@@ -1,2 +1,53 @@
 "use strict"
 
+var enterTask = document.querySelector('.enterTask');
+var addTask = document.querySelector('.addTask');
+var card = document.querySelector('.card');
+
+class task{
+    constructor(name){
+        this.createTask(name);
+    }
+
+    createTask(name){
+        let itemBox = document.createElement('div');
+        itemBox.classList.add('TODOitem');
+
+        let input = document.createElement('input');
+    	input.type = "text";
+    	input.disabled = true;
+    	input.value = name;
+    	input.classList.add('item');
+
+    	var edit = document.createElement('button');
+    	edit.classList.add('itemCancel');
+    	edit.innerHTML = "Can";
+    	edit.addEventListener('click', () => this.edit(input));
+
+    	var remove = document.createElement('button');
+    	remove.classList.add('itemDone');
+    	remove.innerHTML = "Do";
+    	remove.addEventListener('click', () => this.remove(itemBox));
+
+    	card.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(edit);
+        itemBox.appendChild(remove);
+    }
+    edit(input){
+      
+           input.disabled = !input.disabled;
+
+    	
+    }
+
+    remove(itemBox){
+        itemBox.parentNode.removeChild(itemBox);
+     }
+}
+
+
+addTask.addEventListener('click',()=>new task(enterTask.value));
+
+
