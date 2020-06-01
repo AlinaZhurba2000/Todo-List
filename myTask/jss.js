b9ab2fdb44682b5371630495cb66ad2f3a751a12
AlinaZhurba2000 <zhurbaalina@mail.ru>
2020-06-01 17:26:36 +0300

    sdsd

--- myTask/jss.js
+++ myTask/jss.js
@@ -7,57 +7,30 @@ let chousePriority = document.querySelector('.chouseSort');
 
 let Tasks = [];
 
-let sortByName = (Tasks) => {
-    return Tasks.sort((a,b)=>a.value>b.value ? 1: -1);
-}
-
 function createTaskElement(name,priority){
     this.name = name;
     this.priority = priority;
     this.status = false;
 }
 
-function pressAdd()
-{
+addTask.onclick = () => {
+     
     //добавляем задачу в массив
-   // let task = new createTaskElement(enterTask.value,chousePriority.value);
+    let task = new createTaskElement(enterTask.value,chousePriority.value);
     
-   Tasks.push(enterTask.value);
-   
-   let mapped = Tasks.map(
-        (task, i) => ({
-            index: i,
-            name: task.toLowerCase(),
-        })
-    );
-
-    let sortedTasks = sortByName(mapped).map(task=>Tasks[task.index]);
-
-    todolist.innerHTML = sortedTasks.map(task => '<li>' + task + "</li>").join('')
-    enterTask.value = '';
-
-
-}
-
-
-
-   /* //считаем количество задач
-    let getCountofTask = document.querySelector('.count');
-    getCountofTask.value = Tasks.length;
-    getCountofTask.classList.add('count');
-    
-
     //создаем карточку для задачи
-    let divTask = document.createElement('div');
-    divTask.classList.add('TODOitem');
-    card.appendChild(divTask);
+     let divTask = document.createElement('div');
+     divTask.classList.add('TODOitem');
+     card.appendChild(divTask);
 
-    //текст задачи
+     //текст задачи
     let poleTask = document.createElement('input');
     poleTask.classList.add('item');
-    poleTask.type = 'text';
+    poleTask.type ='text';
     poleTask.value = 'priority:' + task.priority +'  ' + task.name ;
+    poleTask.name = task.priority;
     poleTask.disabled = true;
+    //poleTask.addEventListener('click',()=>editTask(poleTask));
     divTask.appendChild(poleTask);
 
     //кнопка удаления
@@ -67,21 +40,42 @@ function pressAdd()
     deleteButton.addEventListener('click',()=>deleteTask(divTask, Tasks));
     divTask.appendChild(deleteButton);
 
-    //удаление элемента
-function deleteTask(divTask, Tasks)
-{
-    divTask.parentNode.removeChild(divTask);
-    //добавить сюда удаление из массива
-    alert(Tasks.length);
-    //Tasks.splice(task);
-    
-   
-    
-}
+    let editButton = document.createElement('button');
+    editButton.classList.add('itemCancel');
+    editButton.innerHTML='Edit';
+    editButton.addEventListener('click',()=>editTask(poleTask));
+    divTask.appendChild(editButton);
+
+    //считаем количество задач
+    let getCountofTask = document.querySelector('.count');
+    getCountofTask.value = card.children.length-1;
+    getCountofTask.classList.add('count');
 
     enterTask.value="";
+};
+    
+//удаление элемента
+function deleteTask(divTask)
+{
+    divTask.parentNode.removeChild(divTask);
+
+    //считаем количество задач
+    let getCountofTask = document.querySelector('.count');
+    getCountofTask.value = card.children.length-1;
+    getCountofTask.classList.add('count');
 }
 
-*/
+//почему не работает? хммм...
+function editTask(poleTask)
+{
+    poleTask.disabled = !poleTask.disabled;
+}
+
+let sortHigh = document.querySelector("#High");
+
+
+
+
+
 
 
