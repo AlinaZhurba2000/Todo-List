a360a42ed945171df2b8f92ed1c58d7be1b99d4a
s1nkara <smartovickiy@gmail.com>
2020-06-17 19:06:14 +0300

    Посмотрел код, дал комментарии по некоторым моментам

--- myTask/todoJS.js
+++ myTask/todoJS.js
@@ -1,10 +1,10 @@
 "use strict"
-
+// todo XHR запросы ещё не реализованы
 let enterTask = document.querySelector('.enterTask');
 let addTask = document.querySelector('.addTask');
 let card = document.querySelector('.card');
 let chousePriority = document.querySelector('.chouseSort');
-
+//todo для чего нужен массив Tasks и где применяется не нашёл
 let Tasks = [];
 
 function createTaskElement(name,priority,status){
@@ -14,14 +14,15 @@ function createTaskElement(name,priority,status){
 }
 
 addTask.onclick = () => {
-     
     //добавляем задачу в массив
+    // todo почему массив, разве это не объект?
     let task = new createTaskElement(enterTask.value,chousePriority.value);
     
     //создаем карточку для задачи
      let divTask = document.createElement('div');
      divTask.classList.add('TODOitem');
      divTask.innerHTML='Priority: ' + task.priority;
+     // todo за id карточки принимается приоритет?
      divTask.id = task.priority;
      card.appendChild(divTask);
 
@@ -32,17 +33,18 @@ addTask.onclick = () => {
     poleTask.value = task.name ;
     poleTask.name = task.priority;
     poleTask.disabled = true;
-    
     divTask.appendChild(poleTask);
 
     //кнопка удаления
     let deleteButton = document.createElement('button');
     deleteButton.classList.add('itemDelete');
     deleteButton.innerHTML='Delete';
+    //todo Tasks передаётся, но никаких манипуляций с ним нет в функции
     deleteButton.addEventListener('click',()=>deleteTask(divTask, Tasks));
     divTask.appendChild(deleteButton);
 
     //пока не знаю, как сделать редактируемым по нажатию (почему-то не выходит)
+    //todo не совсем понял, почему не выходит, если нужно было активировать редактирование, то это работает, но нужно ведь сохранить изменение или отменить(опять же к XHR возвращаемся)
     let editButton = document.createElement('button');
     editButton.classList.add('itemCancel');
     editButton.innerHTML='Edit';
@@ -113,7 +115,9 @@ sortLow.addEventListener('click',()=>{
     .sort((divA,divB)=>divA.name>divB.name ? 1 :-1);
 
 card.append(...sortedDivByLow);
-})
+});
+
+//todo пока реализованы только манипуляции с DOM без привязки к серверу
 
 //1. исправить сортировку
 //2. добавить дату
